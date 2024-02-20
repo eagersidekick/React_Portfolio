f42b9a09bd08df09cce8746c6c19b3de0d3c2b40
Heather Crawford <heather.crawford.ela@gmail.com>
2024-02-19 22:02:15 -0600

    added about me and began portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,11 +6,12 @@ export default function Portfolio() {
         <div class="row">
           <div class="col-sm">
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap" />
+              <img class="card-img-top" src="src\assets\images\Screenshot 2024-02-19 215657.png" alt="Card image cap" />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Note Taker</h5>
+                <p class="card-text">A notetaking application.</p>
+                <a href="https://vast-bayou-91858-1f9d9d9478c9.herokuapp.com/" class="btn btn-success">Visit Site</a>
+                <a href="https://github.com/eagersidekick/Note_Taker" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
@@ -20,17 +21,19 @@ export default function Portfolio() {
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-success">Visit Site</a>
+                <a href="#" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
           <div class="col-sm">
           <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap" />
+              <img class="card-img-top" src="src\assets\images\passwordgnrtr.png" alt="Card image cap" />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Password Generator</h5>
+                <p class="card-text">Generate a random password with javascript.</p>
+                <a href="https://eagersidekick.github.io/Random_Password_Generator/" class="btn btn-success">Visit Site</a>
+                <a href="https://github.com/eagersidekick/Random_Password_Generator#" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
@@ -43,7 +46,8 @@ export default function Portfolio() {
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-success">Visit Site</a>
+                <a href="#" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
@@ -53,17 +57,19 @@ export default function Portfolio() {
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-success">Visit Site</a>
+                <a href="#" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
           <div class="col-sm">
           <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap" />
+              <img class="card-img-top" src="src\assets\images\portal.png" alt="Card image cap" />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">ClockWise</h5>
+                <p class="card-text">An MVC timecard App.</p>
+                <a href="#" class="btn btn-success">Visit Site</a>
+                <a href="#" class="btn btn-success">Go to Repository</a>
               </div>
             </div>
           </div>
